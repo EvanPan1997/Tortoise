a2f3210ad298022f40a04a74fa7c6ac383371df7
yifei.pan <3578313864@qq.com>
2022-11-10 17:56:24 +0800

    init project

---
+++ client/src/tortoise.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// init main
+
+	// init connection
+	/*
+		proto := "tcp"
+		ip_addr := "127.0.0.1"
+		port := "9999"
+		target := ip_addr + ":" + port
+		conn, err := net.Dial(proto, addr)
+		if (err!=nil) {}
+	*/
+
+	// receive command
+	// analysis command keyword
+	// client process(local) / server process(socket)
+}
