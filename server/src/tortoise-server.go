23ce3eba8bade3bd361dff40a17c71ac65131276
yifei.pan <3578313864@qq.com>
2022-11-12 17:45:38 +0800

    update listener(init version)

--- server/src/tortoise-server.go
+++ server/src/tortoise-server.go
@@ -1,6 +1,43 @@
 package main
 
+import (
+	"log"
+	"net"
+)
+
+func init() {
+
+}
+
 func main() {
-	// init main
 	// define listener
+	listener, err := net.Listen("tcp", "127.0.0.1:56701")
+	if err != nil {
+		log.Fatalln("tortoise server start error: ", err)
+		return
+	}
+	//defer listener.Close()
+	log.Println("tortoise server is starting...")
+
+	for {
+		c, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("accept error:", err)
+			//break
+		}
+		go handleConnection(c)
+	}
+}
+
+func handleConnection(c net.Conn) {
+	defer c.Close()
+	for {
+		var buf = make([]byte, 10)
+		read, err := c.Read(buf)
+		if err != nil {
+			log.Println("conn read error:", err)
+			return
+		}
+		log.Printf("read %d bytes, content is %s\n", read, string(buf[:read]))
+	}
 }
