f191c0d55d3590dfca107938fe6cdb3287211834
yifei.pan <3578313864@qq.com>
2022-12-09 14:39:01 +0800

    update handler example

--- server/src/tortoise-server.go
+++ server/src/tortoise-server.go
@@ -2,42 +2,20 @@ package main
 
 import (
 	"log"
-	"net"
+	"net/http"
 )
 
-func init() {
-
-}
-
 func main() {
-	// define listener
-	listener, err := net.Listen("tcp", "127.0.0.1:56701")
-	if err != nil {
-		log.Fatalln("tortoise server start error: ", err)
-		return
-	}
-	//defer listener.Close()
-	log.Println("tortoise server is starting...")
-
-	for {
-		c, err := listener.Accept()
-		if err != nil {
-			log.Fatalln("accept error:", err)
-			//break
-		}
-		go handleConnection(c)
-	}
+	// following the path, choose a handler
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":5678", nil))
 }
 
-func handleConnection(c net.Conn) {
-	defer c.Close()
-	for {
-		var buf = make([]byte, 10)
-		read, err := c.Read(buf)
-		if err != nil {
-			log.Println("conn read error:", err)
-			return
-		}
-		log.Printf("read %d bytes, content is %s\n", read, string(buf[:read]))
+func handler(w http.ResponseWriter, r *http.Request) {
+	// code|message|payload(need to encode)
+	responseBody := "{\"code\":\"200\",\"message\":\"ok\"}"
+	_, err := w.Write([]byte(responseBody))
+	if err != nil {
+		return
 	}
 }
