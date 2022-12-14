6c1f9b57a0e4cc1399ef976ceef59093643edc05
yifei.pan <3578313864@qq.com>
2022-12-14 17:57:14 +0800

    update: socket init

--- client/src/tortoise.go
+++ client/src/tortoise.go
@@ -1,19 +1,104 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"unsafe"
+)
+
 func main() {
-	// init main
+	// Client Initialize-客户端初始化
+	var (
+		defaultUser string = "root"
+		defaultHost string = "127.0.0.1"
+		defaultPort int    = 9890
 
-	// init connection
-	/*
-		proto := "tcp"
-		ip_addr := "127.0.0.1"
-		port := "9999"
-		target := ip_addr + ":" + port
-		conn, err := net.Dial(proto, addr)
-		if (err!=nil) {}
-	*/
+		user     string
+		password string
+		host     string
+		port     int
+	)
+	// Receive Arguments From Command Line-接收命令行传参
+	flag.StringVar(&user, "u", defaultUser, "")
+	flag.StringVar(&host, "h", defaultHost, "")
+	flag.IntVar(&port, "p", defaultPort, "")
+	// Analysis Arguments-参数解析
+	flag.Parse()
 
-	// receive command
-	// analysis command keyword
-	// client process(local) / server process(socket)
+	// Receive Password To Login-接收密码用于登录
+	n := 0
+	for password == "" {
+		if n <= 2 {
+			fmt.Printf("Please enter the password: ")
+			_, err := fmt.Scan(&password)
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(-1)
+			}
+		} else {
+			os.Exit(-1)
+		}
+		n++
+	}
+	// Check The Length of Password-校验密码长度
+	if len(password) < 8 {
+		fmt.Println("The length of password is too low")
+		os.Exit(-1)
+	}
+	// Create Dail To Login-创建Tcp连接并登录
+	ip, err := stringToIP(host)
+	if err != nil {
+		return
+	}
+	raddr := net.TCPAddr{IP: ip, Port: port}
+	tcpConn, err := net.DialTCP("tcp4", nil, &raddr)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
+
+	_, err = tcpConn.Write(stringToBytes("hello"))
+	if err != nil {
+		return
+	}
+	// Cycling and Receiving Commands-循环接收指令
+}
+
+func stringToIP(host string) (net.IP, error) {
+	section := strings.Split(host, ".")
+	if len(section) != 4 {
+		return nil, errors.New("host format error:" + host)
+	}
+	a, err := strconv.Atoi(section[0])
+	if err != nil {
+		return nil, err
+	}
+	b, err := strconv.Atoi(section[1])
+	if err != nil {
+		return nil, err
+	}
+	c, err := strconv.Atoi(section[2])
+	if err != nil {
+		return nil, err
+	}
+	d, err := strconv.Atoi(section[3])
+	if err != nil {
+		return nil, err
+	}
+	return net.IPv4(byte(a), byte(b), byte(c), byte(d)), nil
+}
+
+func stringToBytes(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Len = sh.Len
+	bh.Cap = sh.Len
+	return b
 }
--- server/src/tortoise-server.go
+++ server/src/tortoise-server.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
 	// default enter, analysis path and choose an available handler
-	laddr := &net.TCPAddr{
+	laddr := net.TCPAddr{
 		IP:   nil,
 		Port: 9890,
 	}
-	tcpListener, err := net.ListenTCP("tcp4", laddr)
+	tcpListener, err := net.ListenTCP("tcp4", &laddr)
 	if err != nil {
 		return
 	}
@@ -19,10 +21,16 @@ func main() {
 		if err != nil {
 			return
 		}
+		tcpConn.SetKeepAlive(true)
+		tcpConn.SetKeepAlivePeriod(time.Minute * 5)
+		tcpConn.SetLinger(10)
 		go handlerConnection(tcpConn)
 	}
 }
 
-func handlerConnection(listener *net.TCPConn) {
-
+func handlerConnection(tcpConn *net.TCPConn) {
+	fmt.Println("success")
+	// 1.Verify Login
+	// 2.Process Request
+	// 3.Exit
 }
